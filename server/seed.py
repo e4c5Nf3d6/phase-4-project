5bade954b4f90baa497b46ab791f924ace2a91dd
Maria Jenkins <macjenkin@gmail.com>
2024-02-19 07:44:25 -0600

    create user model

--- server/seed.py
+++ server/seed.py
@@ -3,9 +3,38 @@ from random import randint, choice as rc
 from faker import Faker
 
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
-    fake = Faker()
+
+    # fake = Faker()
+
     with app.app_context():
-        print("Seeding database...")
+        
+        print("Deleting records...")
+        User.query.delete()
+
+        fake = Faker()
+
+        print('Creating users...')
+
+        users = []
+        usernames = []
+
+        for i in range(20):
+            username = fake.first_name()
+            while username in usernames:
+                username = fake.first_name()
+            usernames.append(username)
+
+            user = User(
+                username=username
+            )
+
+            user.password_hash = user.username + 'password'
+
+            users.append(user)
+
+        db.session.add_all(users)
+        db.session.commit()
+        print("Database seeded.")
