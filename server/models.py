b768155b1239303b38a19547b715a5a5377a1c86
Maria Jenkins <macjenkin@gmail.com>
2024-02-19 21:30:15 -0600

    add Game model

--- server/models.py
+++ server/models.py
@@ -7,13 +7,21 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    serialize_rules = ('-players.user',)
+    serialize_rules = (
+        '-players.user',
+        '-players.games_with_white',
+        '-players.games_with_black',
+        '-games.user',
+        '-games.white_player',
+        '-games.black_player'
+    )
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     _password_hash = db.Column(db.String)
 
     players = db.relationship('Player', back_populates="user")
+    games = db.relationship('Game', back_populates="user")
 
     @hybrid_property
     def password_hash(self):
@@ -30,10 +38,45 @@ class User(db.Model, SerializerMixin):
 class Player(db.Model, SerializerMixin):
     __tablename__ = 'players'
 
-    serialize_rules = ('-user.players',)
+    serialize_rules = (
+        '-user.players', 
+        '-user.games',
+        '-games_with_white.white_player', 
+        '-games_with_white.black_player',
+        '-games_with_white.user',
+        '-games_with_black.white_player', 
+        '-games_with_black.black_player',
+        '-games_with_black.user'
+    )
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     user = db.relationship('User', back_populates='players')
+    games_with_white = db.relationship('Game', primaryjoin="Player.id==Game.white_player_id", back_populates='white_player')
+    games_with_black = db.relationship('Game', primaryjoin="Player.id==Game.black_player_id", back_populates='black_player')
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ ='games'
+
+    serialize_rules = (
+        '-user.games', 
+        '-user.players',
+        '-white_player.games_with_white', 
+        '-white_player.games_with_black',
+        '-white_player.user',
+        '-black_player.games_with_white', 
+        '-black_player.games_with_black',
+        '-black_player.user'
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    pgn = db.Column(db.String, nullable=False)
+    white_player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    black_player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', back_populates='games')
+    white_player = db.relationship('Player', foreign_keys=[white_player_id], back_populates='games_with_white')
+    black_player = db.relationship('Player', foreign_keys=[black_player_id], back_populates='games_with_black')
--- server/seed.py
+++ server/seed.py
@@ -3,7 +3,7 @@ from random import randint, choice as rc
 from faker import Faker
 
 from app import app
-from models import db, User, Player
+from models import db, User, Player, Game
 
 if __name__ == '__main__':
 
@@ -12,6 +12,7 @@ if __name__ == '__main__':
         print("Deleting records...")
         User.query.delete()
         Player.query.delete()
+        Game.query.delete()
 
         fake = Faker()
 
@@ -19,6 +20,11 @@ if __name__ == '__main__':
 
         users = []
         usernames = []
+
+        maria = User(username='Maria')
+        maria.password_hash = 'chess'
+        usernames.append('Maria')
+        users.append(maria)
 
         for i in range(20):
             username = fake.first_name()
@@ -40,11 +46,22 @@ if __name__ == '__main__':
 
         players = []
 
+        kasparov = Player(
+            name="Garry Kasparov",
+            user_id=1
+        )
+        players.append(kasparov)
+
+        topalov = Player(
+            name="Veselin Topalov",
+            user_id=1
+        )
+        players.append(topalov)
+
         names = [
                     "Anatoly Karpov", 
                     "Aron Nimzowitsch",
                     "Fabiano Caruana",
-                    "Garry Kasparov", 
                     "Magnus Carlsen", 
                     "Miguel Najdorf",
                     "Mikhail Tal",
@@ -56,12 +73,49 @@ if __name__ == '__main__':
         for name in names:
             player = Player(
                 name=name,
-                user_id=randint(1, 20)
+                user_id=randint(2, 21)
             )
 
             players.append(player)
 
         db.session.add_all(players)
+
+        print("Creating games...")
+
+        games = []
+
+        game1 = Game(
+            pgn='''
+                [Event "Hoogovens Group A"]
+                [Site "Wijk aan Zee NED"]
+                [Date "1999.01.20"]
+                [EventDate "1999.01.16"]
+                [Round "4"]
+                [Result "1-0"]
+                [White "Garry Kasparov"]
+                [Black "Veselin Topalov"]
+                [ECO "B07"]
+                [WhiteElo "2812"]
+                [BlackElo "2700"]
+                [PlyCount "87"]
+
+                1. e4 d6 2. d4 Nf6 3. Nc3 g6 4. Be3 Bg7 5. Qd2 c6 6. f3 b5
+                7. Nge2 Nbd7 8. Bh6 Bxh6 9. Qxh6 Bb7 10. a3 e5 11. O-O-O Qe7
+                12. Kb1 a6 13. Nc1 O-O-O 14. Nb3 exd4 15. Rxd4 c5 16. Rd1 Nb6
+                17. g3 Kb8 18. Na5 Ba8 19. Bh3 d5 20. Qf4+ Ka7 21. Rhe1 d4
+                22. Nd5 Nbxd5 23. exd5 Qd6 24. Rxd4 cxd4 25. Re7+ Kb6
+                26. Qxd4+ Kxa5 27. b4+ Ka4 28. Qc3 Qxd5 29. Ra7 Bb7 30. Rxb7
+                Qc4 31. Qxf6 Kxa3 32. Qxa6+ Kxb4 33. c3+ Kxc3 34. Qa1+ Kd2
+                35. Qb2+ Kd1 36. Bf1 Rd2 37. Rd7 Rxd7 38. Bxc4 bxc4 39. Qxh8
+                Rd3 40. Qa8 c3 41. Qa4+ Ke1 42. f4 f5 43. Kc1 Rd2 44. Qa7 1-0
+            ''',
+            white_player_id=1,
+            black_player_id=2,
+            user_id=1
+        )
+        games.append(game1)
+
+        db.session.add_all(games)
 
         db.session.commit()
         print("Database seeded.")
